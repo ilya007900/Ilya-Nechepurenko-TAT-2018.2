bf2d2249891613a642afbb19a75aad00e5a20ac5
Илья Нечепуренко <ilya00790099@gmail.com>
2018-10-24 12:46:47 +0300

    Add LW_Stack

---
+++ LW-Stack/LW-Stack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Stack<int> stack = new Stack<int>();
+                for (int i = 0; i < 10; i++)
+                {
+                    stack.Push(i);
+                }
+                for (int i = stack.Size; i != 0; i--)
+                {
+                    Console.WriteLine(stack.Pop());
+                }
+                Console.WriteLine(stack.Pop());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
