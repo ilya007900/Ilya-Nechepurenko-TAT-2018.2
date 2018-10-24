5e9779b552a7e299885584b9b717d1344476c2aa
Илья Нечепуренко <ilya00790099@gmail.com>
2018-10-24 12:42:51 +0300

    Add third task

---
+++ DEV-3/DEV-3/DEV3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DEV_3
+{
+    class DEV3
+    {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args">arguments from command line</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("Not enough parameters");
+                    return;
+                }
+                if (args.Length > 2)
+                {
+                    Console.WriteLine("Too many parameters");
+                    return;
+                }
+                if (!(int.TryParse(args[0], out int number)))
+                {
+                    Console.WriteLine("First parameter is not integer or number");
+                    return;
+                }
+                if (!(int.TryParse(args[1], out int system)))
+                {
+                    Console.WriteLine("Second parameter is not integer or number");
+                    return;
+                }
+                DecimalIntConverter intConverter = new DecimalIntConverter();
+                Console.WriteLine(intConverter.ConvertToNewSystem(number, system));
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ DEV-3/DEV-3/DecimalIntConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_3
+{
+    /// <summary>
+    /// Class that provides methods to convert deciaml int in the new number system 
+    /// </summary>
+    class DecimalIntConverter
+    {
+        /// <summary>
+        /// Converts deciaml int in the new number system 
+        /// </summary>
+        /// <param name="number">number for converting</param>
+        /// <param name="system">new number system</param>
+        /// <returns>number as string in the new number system</returns>
+        public string ConvertToNewSystem(int number, int system)
+        {
+            if (system < 2 || system > 20) 
+            {
+                throw new Exception("Error number of system");
+            }
+
+            bool isPositive = number >= 0;
+            if (!isPositive)
+            {
+                number = Math.Abs(number);
+            }
+            string symbolsOfSystems = "0123456789ABCDEFGHIJ";
+            StringBuilder reverseResult = new StringBuilder();
+
+            do
+            {
+                reverseResult.Append(symbolsOfSystems[number % system]);
+                number /= system;
+            }
+            while (number != 0);
+
+            if (!isPositive)
+            {
+                reverseResult.Append('-');
+            }
+
+            StringBuilder result = new StringBuilder(reverseResult.Length);
+
+            for (int i = reverseResult.Length - 1; i >= 0; i--) 
+            {
+                result.Append(reverseResult[i]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
