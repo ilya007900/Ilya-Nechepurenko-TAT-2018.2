f184ec4ce756c1178eb071f1cf7537b2adbf2ebd
Илья Нечепуренко <ilya00790099@gmail.com>
2018-10-27 18:07:38 +0300

    Add XmlFileReader class

---
+++ DEV-4/DEV-4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DEV_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = "file.xml";
+                XmlFileReader fileReader = new XmlFileReader();
+                string stringXml = fileReader.ReadXmlFile(path);
+                Console.WriteLine(stringXml);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ DEV-4/DEV-4/XmlFileReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DEV_4
+{
+    class XmlFileReader
+    {
+        public string ReadXmlFile(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            if (!fileInfo.Exists)
+            {
+                throw new Exception("Can not read file");
+            }
+            else
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
