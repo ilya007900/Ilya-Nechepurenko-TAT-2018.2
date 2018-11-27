ddedaaebec32f519cb7b04c275f2c8ccaf5804c5
Илья Нечепуренко <ilya00790099@gmail.com>
2018-11-27 15:35:02 +0300

    Changes...

---
+++ DEV-5/DEV-5/CarGetter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DEV_5
+{
+    static class CarGetter
+    {
+        public static Car GetCarFromConsole()
+        {
+            Console.WriteLine("Input car brand");
+            string brand = Console.ReadLine();
+
+            Console.WriteLine("Input car model");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("Input car price");
+            int price = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input car count");
+            int count = int.Parse(Console.ReadLine());
+
+            return new Car(brand, model, count, price);
+        }
+    }
+}
