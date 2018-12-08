9a11152699bf286b52009bf917ec4b47df70d8a9
Илья Нечепуренко <ilya00790099@gmail.com>
2018-12-08 13:07:10 +0300

    Task 3 Not all

---
+++ ControlWork/Task3/Task3/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    class Product
+    {
+        public string Type { get; set; }
+        public string Price { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+    }
+}
